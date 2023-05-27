513c274cd966b23c3805ebdb77cbb6c280a77253
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-26 21:11:40 -0700

    comments added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Routes from './components/Routes';
 
+// we can use useContext so our children components can have access to same state without having
+// to pass props to all components
 function App() {
   return (
     <div className="App">
