e03011f88a8f0b17b4d0f77adb4f7b405ff47fb5
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-26 21:15:15 -0700

    NewColor is passed a function to update colors array (state), Colors and Color component are both passed the array of colors ( we can use useContext to avoid this )

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import NewColor from './NewColor';
 import Colors from './Colors';
 import Color from './Color';
 
 export default function Routes() {
+    const [colors, setColors] = useState(['da3e3e', '3b09ae', '49e71d']);
+
     return (
         <Switch>
-            <Route exact path='/colors/new/'><NewColor /></Route>
-            <Route exact path='/colors/:color/'><Color /></Route>
-            <Route exact path='/colors/'><Colors /></Route>
+            <Route exact path='/colors/'><Colors colors={colors}/></Route>
+            <Route exact path='/colors/new/'><NewColor submit={setColors}/></Route>
+            <Route exact path='/colors/:slug/'><Color colors={colors}/></Route>
             <Redirect to='/colors/' />
         </Switch>
     )
