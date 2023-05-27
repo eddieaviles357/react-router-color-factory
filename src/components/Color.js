59521707d2117355e9e098ea5c96c855b2c63c50
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-26 21:12:30 -0700

    Color will now check the slug against colors array, if the slug is not valid we will redirect to /colors page

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { NavLink } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 
-export default function Color(props) {
+export default function Color({colors}) {
+    const {slug} = useParams();
+    const history = useHistory();
+
+    useEffect(() => {
+        const validSlug = colors.includes(slug);
+        // is slug in the colors array is it valid
+        if(!validSlug) { // redirect to home /colors
+            history.push('/colors');
+        };
+
+    }, [slug, history, colors])
     return (
-        <div>
-            Color
+        <div className='Color Flex-column' style={{backgroundColor: `#${slug}`}}>
+            <h1>Beautiful color</h1>
+            <h3>color: {slug}</h3>
             <NavLink to='/colors'>Go Back</NavLink>
         </div>
     )
