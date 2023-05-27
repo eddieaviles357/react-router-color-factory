a370c7d0496aa3f8e81dede06b1526917bd89c76
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-26 21:13:48 -0700

    git form will update when changed, when submitted we will append to colors array

--- src/components/NewColor.js
+++ src/components/NewColor.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
 
-export default function NewColor() {
+export default function NewColor({submit}) {
+    const [clr, setClr] = useState('#ffffff')
+    const history = useHistory();
 
     const handleSubmit = (evt) => {
         evt.preventDefault();
-        console.log(evt)
+        // add color but remove the # character
+        submit( prev => [...prev, clr.slice(1)]);
+        history.push('/colors');
+    }
+
+    const handleChange = (evt) => {
+        setClr(() => evt.target.value)
     }
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <label for='color'>Color</label>
-                <input type='color' name='color' />
-                <button></button>
+            <form className='Form' onSubmit={handleSubmit}>
+                <label htmlFor='color'>Select Color</label>
+                <input type='color' name='color' onChange={handleChange} value={clr}/>
+                <button>Submit</button>
             </form>
         </>
     )
