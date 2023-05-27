905835428e71f87327f36bbcb3e081dc62335622
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-26 19:50:44 -0700

    new components added

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function Colors() {
     return (
         <>
+        <nav className='Nav'>
         <h1>Welcome to the color factory</h1>
-        
+        <NavLink exact to='/colors/new'>Add a color</NavLink>
+        </nav>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <h3>Please select a color</h3>
+        {}
         </>
     )
 }
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import NewColor from './NewColor';
+import Colors from './Colors';
+import Color from './Color';
 
 export default function Routes() {
     return (
         <Switch>
-            <Route exact path='/colors'>
-                
-            </Route>
-            <Route exact path='/colors/:color'>
-
-            </Route>
-            <Redirect to='/colors' />
+            <Route exact path='/colors/new/'><NewColor /></Route>
+            <Route exact path='/colors/:color/'><Color /></Route>
+            <Route exact path='/colors/'><Colors /></Route>
+            <Redirect to='/colors/' />
         </Switch>
     )
 }
